bd95988c891fd18e86abf666fedaa1f78ef44c10
probit @ 3520 <probitlabs@protonmail.com>
2018-02-11 18:15:18 +0100

    chromium: Scratchin fishy builder, Listin native deps

--- src/chromium-way/common.nix
+++ src/chromium-way/common.nix
@@ -1,4 +1,6 @@
 {
+    info,
+
     product ? "chromium-way",
     channel ? "stable",
 
@@ -16,20 +18,49 @@ let
     };
     deps = with pkgs; [
         # Tooling
-        yasm bison gperf kerberos icu re2
+        yasm bison gperf kerberos icu re2 libgcrypt
         # Networking
         libnss kerberos
         # Compression
         bzip2 zlib snappy zlib minizip
         # Audio
-        flac opusWithCustomModes speex
+        libpulseaudio alsaLib flac opusWithCustomModes speex
         # Graphics
         libpng libjpeg libwebp ffmpeg
         # Data
         xdg_utils libxslt libxml2
         # Interface
-        libpulseaudio dbus
+        dbus
         # Hardware
-        libpci libcap libusb1
-    ]
-in rec {}
+        libpci libcap libusb1 cups
+    ];
+    suffix = if channel == "stable" then "" else "-${channel}";
+    metadata = {
+        description = "Google's open source browser built for Wayland";
+        homepage = https://git.io/ozone;
+        license = pkgs.licenses.bsd3;
+        platforms = pkgs.platforms.linux;
+    };
+in rec {
+    name = product + suffix + "-" + version;
+    inherit (metadata) description homepage license platforms;
+
+    inherit (info) src version;
+    inherit (pkgs) fish;
+    builder = builtins.toFile "builder.sh" ''
+        #!$fish
+        source $stdenv/setup
+
+        source <(echo $configurePhase)
+        source <(echo $buildPhase)
+    '';
+    configurePhase = ''
+        python tools/gn/bootstrap/bootstrap.py -v -s --no-clean
+        set -x PATH $PWD/out/Release $PATH
+    '';
+    buildPhase = ''
+    '';
+    nativeBuildInputs = with pkgs // pkgs.python2Packages; [
+        ninja which python perl pkgconfig ply jinja2 nodejs gnutar
+    ];
+}
