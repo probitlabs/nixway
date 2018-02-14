fc34d81452c4f3303b01a28d377f7428298afdc7
probit @ 3520 <probitlabs@protonmail.com>
2018-02-14 22:48:06 +0100

    ozone: Emailin probit as maintainer

--- src/chromium-way/meta.nix
+++ src/chromium-way/meta.nix
@@ -3,6 +3,9 @@ pkgs:
 {
     description = "Google's open source browser built for Wayland";
     homepage = https://git.io/ozonium;
+    maintainers = [
+        "probit Labs <labs@probit.link>"
+    ];
     license = pkgs.lib.licenses.bsd3;
     platforms = pkgs.lib.platforms.linux;
 }
